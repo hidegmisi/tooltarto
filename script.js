08dc0b8a65905ac53ad8355342c9735fc1f3f033
László Horváth <bedafoltyn@gmail.com>
2019-07-26 12:16:52 +0200

    slim-select added, new select styling

--- script.js
+++ script.js
@@ -2,26 +2,25 @@ const typeSelect = $('#type')
 const costSelect = $('#cost')
 const whereSelect = $('#where')
 
+new SlimSelect({
+    select: '#type',
+    showSearch: false
+})
+new SlimSelect({
+    select: '#cost',
+    showSearch: false
+})
+new SlimSelect({
+    select: '#where',
+    showSearch: false
+})
+
 function updateList () {
     const selectedType = typeSelect.find('option:selected').val()
     const selectedCost = costSelect.find('option:selected').val()
     const selectedWhere = whereSelect.find('option:selected').val()
 
     $('.tools li').each(function () {
-        console.log('-----------------')
-        console.log(selectedType)
-        console.log($(this).data('type') === selectedType || selectedType === 'összes')
-
-        console.log(selectedCost)
-        console.log($(this).data('cost') === selectedCost || selectedCost === 'összes')
-
-        console.log(selectedWhere)
-        console.log($(this).data('where') === selectedWhere || selectedWhere === 'összes')
-
-        console.log(($(this).data('type') === selectedType || selectedType === 'összes')
-        && ($(this).data('cost') === selectedCost || selectedCost === 'összes')
-        && ($(this).data('where') === selectedWhere || selectedWhere === 'összes'))
-
         if (($(this).data('type') === selectedType || selectedType === 'összes')
             && ($(this).data('cost') === selectedCost || selectedCost === 'összes' || selectedCost === 'ingyenes/fizetős')
             && ($(this).data('where') === selectedWhere || selectedWhere === 'összes')) {
