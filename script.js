955a1434350db15292c5be6c8aa3f4bbbee37677
László Horváth <bedafoltyn@gmail.com>
2019-07-26 12:30:32 +0200

    updateList refactored

--- script.js
+++ script.js
@@ -15,15 +15,17 @@ new SlimSelect({
     showSearch: false
 })
 
+function isSelected (element, value, selected) {
+    return element.data(value) === selected || selected === 'összes' || selected === 'ingyenes/fizetős'
+}
+
 function updateList () {
     const selectedType = typeSelect.find('option:selected').val()
     const selectedCost = costSelect.find('option:selected').val()
     const selectedWhere = whereSelect.find('option:selected').val()
 
     $('.tools li').each(function () {
-        if (($(this).data('type') === selectedType || selectedType === 'összes')
-            && ($(this).data('cost') === selectedCost || selectedCost === 'összes' || selectedCost === 'ingyenes/fizetős')
-            && ($(this).data('where') === selectedWhere || selectedWhere === 'összes')) {
+        if (isSelected($(this), 'type', selectedType) && isSelected($(this), 'cost', selectedCost) && isSelected($(this), 'where', selectedWhere)) {
             $(this).addClass('active')
         } else {
             $(this).removeClass('active')
